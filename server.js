d4e53394e1f7c14487abe42c9d5061877e493b30
JEREMIAH\LENOVO <jeze5959@gmail.com>
2025-03-20 15:29:29 +0100

    Replaced bcrypt with bcryptjs

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
